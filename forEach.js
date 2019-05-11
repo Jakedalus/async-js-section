35e6092b04ad87ae8e89a7753c9c36257fd91045
Jakedalus <jacquedalus@gmail.com>
2019-05-11 15:08:26 -0400

    Add complex forEach test

--- forEach.js
+++ forEach.js
@@ -11,3 +11,14 @@ forEach(test, function(number) {
   console.log(number * 2);
 });
 
+const msg = ['my', 'forEach', 'example'];
+
+let result = "";
+forEach(msg, function(str, index, arr) {
+  if (arr.length - 1 === index) {
+    result += str + "!!!!";
+  } else {
+    result += str + " ";
+  }
+});
+console.log(result);

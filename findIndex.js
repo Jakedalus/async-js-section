f1a815972a105128d49b135ed0ec7d4db5b89122
Jakedalus <jacquedalus@gmail.com>
2019-05-11 15:17:22 -0400

    Implement findIndex

--- findIndex.js
+++ findIndex.js
@@ -1,23 +1,29 @@
 function findIndex(arr, callback) {
-
+  for(let i = 0; i < arr.length; i++) {
+    if (callback(arr[i], i, arr)) {
+      return i;
+    }
+  }
+  
+  return -1;
 }
 
 const arr1 = [3, 4, 6, 2, 1];
-findIndex(arr1, function(num) {
+console.log(findIndex(arr1, function(num) {
   return num === 6;
-});
+}));  // 2
 
 const arr2 = [5, 11, 13, 8, 6, 7];
-findIndex(arr2, function(num) {
+console.log(findIndex(arr2, function(num) {
   return num % 2 === 0;
-});
+}));  // 3
 
 const langs = ['Java', 'C++', 'Python', 'Ruby'];
-findIndex(langs, function(str) {
+console.log(findIndex(langs, function(str) {
   return str === 'Javascript';
-});
+}));  // -1
 
 const langs2 = ['Java', 'C++', 'Python', 'Ruby', 'Javascript'];
-findIndex(langs2, function(str) {
+console.log(findIndex(langs2, function(str) {
   return str === 'Javascript';
-});
+}));  // 4
